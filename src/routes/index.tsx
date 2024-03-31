7b1b81bd2f15097ce4102f715c9fc469c2fb3e1d
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-03-31 22:43:12 +0530

    Add route for booking appointments with doctors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,12 @@ import App from "../App";
 import BookAppointment from "../pages/book-appointment/book-appointment";
 import Home from "../pages/home/home";
 import ViewLabReports from "../pages/view-labreports/view-labreports";
+import AvailableTimes from "../pages/book-appointment/available-times";
 
 export const getRoutes = () => [
   //{ path: "/", element: <Navigate to="home" /> },
   { path: "/bookappointment", element: <BookAppointment /> },
+  { path: "/bookappointment/doctor", element: <AvailableTimes /> },
   { path: "/home", element: <Home /> },
   { path: "/viewlabreports", element: <ViewLabReports/> },
 ];
