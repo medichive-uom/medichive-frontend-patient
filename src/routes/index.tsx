c351b52671e2c323ea080053e35f3e0a2c0e33d6
hariniHimanshi <gunarathnehmhh.21@itfac.mrt.ac.lk>
2024-04-01 22:22:19 +0530

    Remove unused imports and update routes in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,3 @@
-
-import React from "react";
-import { Navigate } from "react-router-dom";
-import App from "../App";
 import BookAppointment from "../pages/book-appointment/book-appointment";
 import Home from "../pages/home/home";
 import ViewLabReports from "../pages/view-labreports/view-labreports";
